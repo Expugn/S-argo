1e59dfd4612bf153d5e49bd43d85768e85e4f245
Expugn <Expugn@users.noreply.github.com>
2017-09-11 23:34:03 -0700

    Edit Help Menu
    Realized that if S'argo is using a different prefix then it wouldn't be called by @mentioning it.

--- src/main/java/io/github/spugn/Sargo/Functions/Help.java
+++ src/main/java/io/github/spugn/Sargo/Functions/Help.java
@@ -19,32 +19,32 @@ public class Help
         builder.withAuthorIcon(Images.HELP_ICON.getUrl());
 
         String scoutingHelp = "";
-        scoutingHelp += "'@mention **scout**' - View a list of available banners." + "\n";
-        scoutingHelp += "'@mention **scout** p[Page Number]' - View a page in the list of available banners." + "\n";
-        scoutingHelp += "'@mention **scout** [Banner ID]' - View a banner's information." + "\n";
-        scoutingHelp += "'@mention **scout** [Banner ID] [single/s/1]' - Perform a single scout." + "\n";
-        scoutingHelp += "'@mention **scout** [Banner ID] [multi/m/11]' - Perform a multi scout." + "\n";
-        scoutingHelp += "'@mention **scout** [Banner ID] [guaranteed/g/rc]' - Perform a guaranteed scout.";
+        scoutingHelp += "'**scout**' - View a list of available banners." + "\n";
+        scoutingHelp += "'**scout** p[Page Number]' - View a page in the list of available banners." + "\n";
+        scoutingHelp += "'**scout** [Banner ID]' - View a banner's information." + "\n";
+        scoutingHelp += "'**scout** [Banner ID] [single/s/1]' - Perform a single scout." + "\n";
+        scoutingHelp += "'**scout** [Banner ID] [multi/m/11]' - Perform a multi scout." + "\n";
+        scoutingHelp += "'**scout** [Banner ID] [guaranteed/g/rc]' - Perform a record crystal scout.";
         builder.appendField("SCOUT", scoutingHelp, false);
 
         String shopHelp = "";
-        shopHelp += "'@mention **shop**' - View the shop." + "\n";
-        shopHelp += "'@mention **shop** [Item ID]' - \"Buy\" a Memory Diamond bundle." + "\n";
+        shopHelp += "'**shop**' - View the shop." + "\n";
+        shopHelp += "'**shop** [Item ID]' - \"Buy\" a Memory Diamond bundle." + "\n";
         builder.appendField("SHOP", shopHelp, false);
 
         String profileHelp = "";
-        profileHelp += "'@mention **profile**' - View your basic profile." + "\n";
-        profileHelp += "'@mention **profile** [info/i] [Banner ID]' - View your collected/missing characters in a banner." + "\n";
-        profileHelp += "'@mention **profile** [data/d]' - View your Step/Record Crystal data." + "\n";
-        profileHelp += "'@mention **profile** [search/s] [Character Name]' - Search for that character in your collection." + "\n";
+        profileHelp += "'**profile**' - View your basic profile." + "\n";
+        profileHelp += "'**profile** [info/i] [Banner ID]' - View your collected/missing characters in a banner." + "\n";
+        profileHelp += "'**profile** [data/d]' - View your Step/Record Crystal data." + "\n";
+        profileHelp += "'**profile** [search/s] [Character Name]' - Search for that character in your collection." + "\n";
         builder.appendField("PROFILE", profileHelp, false);
 
         String userHelp = "";
-        userHelp += "'@mention **user** [name/@name]' - View a user's basic profile." + "\n";
+        userHelp += "'**user** [name/@name]' - View a user's basic profile." + "\n";
         builder.appendField("USER SEARCH", userHelp, false);
 
         String resetHelp = "";
-        resetHelp += "'@mention **reset**' - Reset your data file." + "\n";
+        resetHelp += "'**reset**' - Reset your data file." + "\n";
         builder.appendField("DATA RESET", resetHelp, false);
 
         CHANNEL.sendMessage(builder.build());
