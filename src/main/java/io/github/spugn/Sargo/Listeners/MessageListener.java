647c1db0a5f097a0b5aad45242210f13cfd26ddd
S'pugn <Expugn@users.noreply.github.com>
2019-08-02 13:25:15 -0700

    v4.0.2 - Fix Issue
    - Direct Messaging the bot will cause it to crash due to an uncaught exception. Now when a user direct messages the bot it will record the message to the console and reply with a message if the user tries to use a command.

--- src/main/java/io/github/spugn/Sargo/Listeners/MessageListener.java
+++ src/main/java/io/github/spugn/Sargo/Listeners/MessageListener.java
@@ -1,55 +1,85 @@
 package io.github.spugn.Sargo.Listeners;
 
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.PrivateChannel;
 import discord4j.core.object.entity.TextChannel;
+import discord4j.core.object.entity.User;
 import io.github.spugn.Sargo.Managers.CommandManager;
+import io.github.spugn.Sargo.Sargo;
 import io.github.spugn.Sargo.XMLParsers.CommandSettingsParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageListener
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageListener.class);
     public static void messageReceived(Message message)
     {
         if (message.getAuthor().isPresent())
         {
             if (!message.getAuthor().get().isBot())
             {
-                String messageChannelName = message.getChannel().cast(TextChannel.class).block().getName();
-                Long messageChannelID = message.getChannelId().asLong();
-
-                // IF THERE IS NOT A MAIN CHANNEL SET...
-                if (CommandSettingsParser.getMainChannel().isEmpty())
+                // DIRECT MESSAGING IS NOT SUPPORTED
+                if (message.getChannel().block() instanceof PrivateChannel)
                 {
-                    // CHECK IF CHANNEL IS BLACKLISTED (NAME OR CHANNEL ID)
-                    boolean isInBlacklistedChannel = CommandSettingsParser.getBlacklistedChannels().contains(messageChannelName) ||
-                            CommandSettingsParser.getBlacklistedChannels().contains(messageChannelID);
+                    User user = message.getAuthor().orElse(null);
+                    String username = user.getUsername() + "#" + user.getDiscriminator();
+                    String receivedMessage = message.getContent().orElse(null);
 
-                    // IF NOT IN BLACKLIST, CONTINUE
-                    if (!isInBlacklistedChannel)
+                    LOGGER.info("Direct Message received from " + username + " (" + user.getId().asString() + "): \"" + receivedMessage + "\"");
+
+                    try
                     {
-                        new CommandManager(message);
+                        if (receivedMessage.startsWith(CommandManager.getCommandPrefix()))
+                        {
+                            Sargo.replyToMessage_Warning(message, "ERROR", "Using commands via **Direct Messaging** is not supported.");
+                        }
+                    }
+                    catch (NullPointerException e)
+                    {
+                        // IGNORED
                     }
                 }
                 else
                 {
-                    // A MAIN CHANNEL IS SET... CHECK IF THE MESSAGE IS SENT FROM THE MAIN CHANNEL.
-                    boolean isInMainChannel;
-                    try
-                    {
-                        isInMainChannel = (messageChannelID == Long.parseLong(CommandSettingsParser.getMainChannel()));
-                    }
-                    catch (NumberFormatException e)
-                    {
-                        isInMainChannel = (messageChannelName.equalsIgnoreCase(CommandSettingsParser.getMainChannel()));
-                    }
+                    String messageChannelName = message.getChannel().cast(TextChannel.class).block().getName();
+                    Long messageChannelID = message.getChannelId().asLong();
 
-                    // CHECK IF THE MESSAGE IS FROM A WHITELISTED CHANNEL AS WELL.
-                    boolean isInWhitelistedChannel = CommandSettingsParser.getWhitelistedChannels().contains(messageChannelName) ||
-                            CommandSettingsParser.getWhitelistedChannels().contains(messageChannelID);
-
-                    // IF MESSAGE IS IN MAIN CHANNEL OR A WHITELISTED ONE, CONTINUE...
-                    if (isInMainChannel || isInWhitelistedChannel)
+                    // IF THERE IS NOT A MAIN CHANNEL SET...
+                    if (CommandSettingsParser.getMainChannel().isEmpty())
                     {
-                        new CommandManager(message);
+                        // CHECK IF CHANNEL IS BLACKLISTED (NAME OR CHANNEL ID)
+                        boolean isInBlacklistedChannel = CommandSettingsParser.getBlacklistedChannels().contains(messageChannelName) ||
+                                CommandSettingsParser.getBlacklistedChannels().contains(messageChannelID);
+
+                        // IF NOT IN BLACKLIST, CONTINUE
+                        if (!isInBlacklistedChannel)
+                        {
+                            new CommandManager(message);
+                        }
+                    }
+                    else
+                    {
+                        // A MAIN CHANNEL IS SET... CHECK IF THE MESSAGE IS SENT FROM THE MAIN CHANNEL.
+                        boolean isInMainChannel;
+                        try
+                        {
+                            isInMainChannel = (messageChannelID == Long.parseLong(CommandSettingsParser.getMainChannel()));
+                        }
+                        catch (NumberFormatException e)
+                        {
+                            isInMainChannel = (messageChannelName.equalsIgnoreCase(CommandSettingsParser.getMainChannel()));
+                        }
+
+                        // CHECK IF THE MESSAGE IS FROM A WHITELISTED CHANNEL AS WELL.
+                        boolean isInWhitelistedChannel = CommandSettingsParser.getWhitelistedChannels().contains(messageChannelName) ||
+                                CommandSettingsParser.getWhitelistedChannels().contains(messageChannelID);
+
+                        // IF MESSAGE IS IN MAIN CHANNEL OR A WHITELISTED ONE, CONTINUE...
+                        if (isInMainChannel || isInWhitelistedChannel)
+                        {
+                            new CommandManager(message);
+                        }
                     }
                 }
             }
